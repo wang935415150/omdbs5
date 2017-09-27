668260cec936f3ccbf0b598df0291701de62d422
wang935415150 <935415150@qq.com>
2017-09-27 19:40:32 +0800

    9/27的19点

--- auto_server/api/views.py
+++ auto_server/api/views.py
@@ -8,5 +8,23 @@ import json
 def server(request):
     ret=request.body
     ret=json.loads(ret.decode('utf-8'))
-    print(ret)
+    hostname=ret['basic']['data']['hostname']
+    server_obj=models.Server.objects.filter(hostname=hostname).first()
+    if not server_obj:
+        server_info={}
+        server_info.update(ret['basic']['data'])
+        server_info.update(ret['board']['data'])
+        server_obj=models.Server.objects.create(**server_info)
+    nic_info=ret['nic']['data']
+    for i in nic_info:
+        nic_dic=nic_info[i]
+        models.NIC.objects.create(name=i,server_obj=server_obj,**nic_dic)
+    disk_info=ret['disk']['data']
+    for i in disk_info:
+        disk_dic=disk_info[i]
+        models.Disk.objects.create(server_obj=server_obj,**disk_dic)
+    memory_info=ret['memory']['data']
+    for i in memory_info:
+        memory_dic=memory_info[i]
+        models.Memory.objects.create(server_obj=server_obj,**memory_dic)
     return HttpResponse("ok")
